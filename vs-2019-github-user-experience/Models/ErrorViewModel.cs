2f3a7d71123d3bc23d15e5864ac7eb27025ad67c
Sladjan Acimovic <sladjan.acimovic@icodefactory.com>
2022-04-17 19:10:16 +0200

    Add project files.

---
+++ vs-2019-github-user-experience/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace vs_2019_github_user_experience.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
